910e21998c6aa5f0ecdc357f7f2f745e6c9d829f
jkroso <jkroso@gmail.com>
2013-06-07 00:12:55 +1200

    add a decorate module

---
+++ decorate.js
@@ -0,0 +1,42 @@
+
+var Promise = require('laissez-faire/full')
+  , rejected = Promise.rejected
+  , coerce = require('./coerce')
+  , when = require('./')
+
+module.exports = function(ƒ){
+	return function(){
+		var args = arguments
+		var len = args.length
+		while (len--) {
+			if (isPromise(args[len])) {
+				var promise = new Promise
+				var next = function(value){
+					args[len] = value
+					if (len) return when(args[--len], next, fail)
+					try {
+						value = ƒ.apply(this, args)
+					} catch (e) {
+						promise.error(e)
+						return
+					}
+					when(value, function(value){
+						promise.write(value)
+					}, fail)
+				}
+				var fail = function(e){
+					promise.error(e)
+				}
+				args[len].then(next, fail)
+				return promise
+			}
+		}
+		try { promise = ƒ.apply(this, arguments) }
+		catch (e) { return rejected(e) }
+		return coerce(promise)
+	}
+}
+
+function isPromise(value){
+	return value && typeof value.then == 'function'
+}
