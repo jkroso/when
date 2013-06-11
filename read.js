4808926e2db8ccf8fa2ea2e7dba7478b2505d6c0
jkroso <jkroso@gmail.com>
2013-06-11 12:18:44 +1200

    Release 2.0.0

--- read.js
+++ read.js
@@ -1,6 +1,4 @@
 
-var Promise = require('laissez-faire/full')
-
 /**
  * read the value of `value` even if its 
  * behind a promise proxy
@@ -12,7 +10,6 @@ var Promise = require('laissez-faire/full')
 
 module.exports = function(value, onValue, onError){
 	if (!value) onValue(value)
-	else if (value instanceof Promise) value.read(onValue, onError)
 	else if (typeof value.then != 'function') onValue(value)
 	else value.then(onValue, onError)
 }
