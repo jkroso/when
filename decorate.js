eb5da0d7f4c00ceeee3ae8840e20434b56c0be95
jkroso <jkroso@gmail.com>
2013-06-20 15:11:45 +1200

    tidy `decorate`

--- decorate.js
+++ decorate.js
@@ -14,31 +14,36 @@ var ResType = require('result-type')
 
 module.exports = function(ƒ){
 	return function(){
-		var args = arguments
-		var len = args.length
-		while (len--) {
-			if (args[len] instanceof ResType) {
-				var result = new Result
-				var next = function(value){
-					args[len] = value
-					if (len) return read(args[--len], next, fail)
-					try {
-						value = ƒ.apply(this, args)
-					} catch (e) {
-						result.error(e)
-						return
-					}
-					read(value, function(value){
+		var i = arguments.length
+
+		// scan for Results
+		while (i--) if (arguments[i] instanceof ResType) {
+			var args = arguments
+			var result = new Result
+			var fail = function(e){
+				result.error(e)
+			}
+			var next = function(value){
+				args[i] = value
+				if (i) return read(args[--i], next, fail)
+
+				// run ƒ
+				try { value = ƒ.apply(this, args) }
+				catch (e) { return result.error(e)}
+
+				// write
+				if (value instanceof ResType) {
+					value.read(function(value){
 						result.write(value)
 					}, fail)
+				} else {
+					result.write(value)
 				}
-				var fail = function(e){
-					result.error(e)
-				}
-				args[len].then(next, fail)
-				return result
 			}
+			args[i].read(next, fail)
+			return result
 		}
+
 		try { result = ƒ.apply(this, arguments) }
 		catch (e) { return failed(e) }
 		return coerce(result)
