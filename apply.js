08894fc32b18ec89012293c91037a037d1b493f9
Jake Rosoman <jkroso@gmail.com>
2013-08-07 13:51:51 +1200

    optimize sexpr

--- apply.js
+++ apply.js
@@ -23,9 +23,15 @@ function apply(){
  */
 
 function sexpr(fn){
-	var ƒ = fn
-	arguments[0] = this
-	return call.apply(ƒ, arguments)
+	switch (arguments.length) {
+		case 2: return fn.call(this, arguments[1])
+		case 3: return fn.call(this, arguments[1], arguments[2])
+		case 4: return fn.call(this, arguments[1], arguments[2], arguments[3])
+		default:
+			var ƒ = fn
+			arguments[0] = this
+			return call.apply(ƒ, arguments)
+	}
 }
 
 module.exports = decorate(apply) 
